ddbba6d7e01fd72543cdd0e18b4750c2a8e22cef
sainadh63042 <sainadh80744@gmail.com>
2023-09-07 16:15:00 +0530

    added generic code

--- restapi_get.py
+++ restapi_get.py
@@ -1,20 +1,15 @@
 from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
-# Sample cricket players data
-cricket_players = [
-    {"id": 1, "name": "Virat Kohli", "country": "India", "role": "Batsman"},
-    {"id": 2, "name": "Kane Williamson", "country": "New Zealand", "role": "Batsman"},
-    {"id": 3, "name": "Joe Root", "country": "England", "role": "Batsman"},
-    # Add more players here
-]
-
+# Load cricket players data from a config file
+with open('cricket_players.json', 'r') as file:
+    cricket_players = json.load(file)
 
 @app.route('/api/cricket/players', methods=['GET'])
 def get_all_cricket_players():
     return jsonify(cricket_players)
-
 
 @app.route('/api/cricket/players/<int:player_id>', methods=['GET'])
 def get_player_by_id(player_id):
@@ -23,7 +18,6 @@ def get_player_by_id(player_id):
         return jsonify(player)
     return jsonify({"message": "Player not found"}), 404
 
-
 @app.route('/api/cricket/players/country/<country>', methods=['GET'])
 def get_players_by_country(country):
     players_in_country = [p for p in cricket_players if p['country'].lower() == country.lower()]
@@ -31,7 +25,5 @@ def get_players_by_country(country):
         return jsonify(players_in_country)
     return jsonify({"message": "No players found for the given country"}), 404
 
-
 if __name__ == '__main__':
     app.run(debug=True)
- 
