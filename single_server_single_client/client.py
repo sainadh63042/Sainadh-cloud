7fa5a0bc4dfdb4ee1545550c2797526c00df55b7
sainadh63042 <126876944+sainadh63042@users.noreply.github.com>
2023-09-08 15:39:19 +0530

    Create client.py

---
+++ single_server_single_client/client.py
@@ -0,0 +1,50 @@
+import socket
+import threading
+import json
+
+with open("config_file.json", "r") as config_file:
+    config_data = json.load(config_file)
+
+
+class Client:
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
+
+    def send_message(self):
+        try:
+            while True:
+                message = input()
+                self.sock.send(message.encode(config_data["format"]))
+                if message.lower() == "bye":
+                    break
+        except Exception as e:
+            print("error: ", str(e))
+        finally:
+            self.sock.close()
+
+    def receive_message(self):
+        try:
+            while True:
+                response = self.sock.recv(config_data["size"]).decode(config_data["format"])
+                print("server: {}".format(response))
+        except Exception as e:
+            print("error: ", str(e))
+        finally:
+            self.sock.close()
+
+
+def main():
+    server_config = config_data["server"]
+    client = Client(server_config["host"], server_config["port"])
+    thread_1 = threading.Thread(target=client.send_message)
+    thread_2 = threading.Thread(target=client.receive_message)
+    thread_1.start()
+    thread_2.start()
+
+
+if __name__ == "__main__":
+    main()
